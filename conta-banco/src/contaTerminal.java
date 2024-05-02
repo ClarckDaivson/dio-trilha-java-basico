8f2925c60b35542624970e3b0856875c3de0e8c3
daivson <daivsonatanaele@gmail.com>
2024-05-01 23:07:56 -0300

    meu projeto conta banco

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      
+        Scanner scanner = new Scanner(System.in);
+     
+        System.out.println("Bem vindo ao ContaBanco");
+        
+        System.out.println("Dgite o numero da agenica");
+        int numeroAgencia = scanner.nextInt();
+     
+
+        System.out.println("Digite o numero da conta");
+        int numerodaConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o nome do titular da conta");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta");
+        double saldoInicial = scanner.nextDouble();
+
+        System.out.println("minha agencia " + numeroAgencia + " conta " + numeroAgencia + " nome "+ nome + " meu saldo é " + saldoInicial);
+        
     }
 }
